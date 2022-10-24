b6874b2834f21c8eec7153ae876184284d36b31a
jherna <jherna76@elpuig.xeill.net>
2022-10-24 15:04:03 +0200

    entities for lambding

---
+++ src/exemples/Cotxe.java
@@ -0,0 +1,19 @@
+package exemples;
+
+public class Cotxe {
+    String marca;
+    public Cotxe(String m) {
+        marca = m;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Cotxe{" +
+                "marca='" + marca + '\'' +
+                '}';
+    }
+}
